69c9966b56b8304ed7ecfb595ed65db556a2bd7b
Rui Barata <rui00barata@MacBook-Air.local>
2022-11-08 15:51:17 +0000

    Added default learn-ocaml

---
+++ learn-ocaml/.ci-macosx.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+sw_vers
+system_profiler SPSoftwareDataType
+uname -a
+
+brew update
+brew install pkg-config
+brew install opam
+brew install libev
+brew install openssl
+opam init -y -a --bare
+
+opam switch create . ocaml-base-compiler --deps-only --locked -y -j 2 # -v
+eval $(opam env)
+make
+make opaminstall
+
+# See src/main/linking_flags.sh
+make detect-libs
